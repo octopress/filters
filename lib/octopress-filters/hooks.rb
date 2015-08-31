6d89a04fd614e9dc7dbf430a355ec883e9108d47
Brandon Mathis <brandon@imathis.com>
2015-08-31 00:36:05 -0500

    Added support for Jekyll3 hooks

--- lib/octopress-filters/hooks.rb
+++ lib/octopress-filters/hooks.rb
@@ -1,18 +1,32 @@
-require 'octopress-hooks'
-
 module Octopress
   module Filters
 
-    class SiteGrabber < Hooks::Site
-      def post_read(site)
+    if defined?(Jekyll::Hooks)
+
+      Jekyll::Hooks.register :site, :post_read do |site|
         Octopress::Filters.site = site
       end
-    end
-
-    class PostHooks < Hooks::Post
-      def pre_render(post)
+      
+      Jekyll::Hooks.register :post, :pre_render do |post, payload|
         excerpted = post.content.match(post.site.config['excerpt_separator'])
-        post.data.merge!({'excerpted' => !!excerpted})
+        payload['excerpted'] = !!excerpted
+      end
+
+    else
+
+      require 'octopress-hooks'
+
+      class SiteGrabber < Hooks::Site
+        def post_read(site)
+          Octopress::Filters.site = site
+        end
+      end
+
+      class PostHooks < Hooks::Post
+        def pre_render(post)
+          excerpted = post.content.match(post.site.config['excerpt_separator'])
+          post.data.merge!({'excerpted' => !!excerpted})
+        end
       end
     end
   end
